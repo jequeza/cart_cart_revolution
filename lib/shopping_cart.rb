c90563b4d900dbf861ba2873181afb31bf0db0ee
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-14 15:52:58 -0500

    Add tests and methods, tests fail out of time.

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -10,4 +10,19 @@ class ShoppingCart
   def add_product(product_add)
     @products << product_add
   end
+
+  def details
+    cart = ShoppingCart.new("King Soopers", "30items")
+    cart_hash = cart.instance_variables.each_with_object({}) {|var, hash| hash[var[1..-2].to_sym] = cart.instance_variable_get(var) }
+    return cart_hash
+  end
+
+  def total_number_of_products
+    products.each do |product|
+      sum += product.quantity
+    end
+    sum
+  end
+
+
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -25,4 +25,21 @@ class ShoppingCartTest < Minitest::Test
     cart.add_product(product2)
     assert_equal [product1, product2], cart.products
   end
+
+  def test_it_can_give_details
+    cart = ShoppingCart.new("King Soopers", "30items")
+    expected = {name: "King Soopers", capacity: 30}
+    assert_equal expected, cart.details
+  end
+
+  def test_it_can_tell_capacity_exceeded
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal 13, cart.total_number_of_products
+  end
 end
